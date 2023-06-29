99e8ee0af16a5e15274b37565ed288ff926cb0ad
Anna <nebesna.kpi@gmail.com>
2023-06-29 14:36:04 +0300

    Initial commit

---
+++ DatingAppAPI/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+using DatingAppAPI.Data;
+using DatingAppAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> GetUser(int id )
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+}
