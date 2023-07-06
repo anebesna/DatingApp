afb8b1551555aa5093bf4eb3fe34deb2602d3c4d
Anna <nebesna.kpi@gmail.com>
2023-07-06 21:47:57 +0300

    Added error handlers and middleware

--- DatingAppAPI/Program.cs
+++ DatingAppAPI/Program.cs
@@ -1,4 +1,5 @@
 using DatingAppAPI.Extensions;
+using DatingAppAPI.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseAuthentication();
 app.UseAuthorization();
--- DatingAppClient/src/app/app-routing.module.ts
+++ DatingAppClient/src/app/app-routing.module.ts
@@ -6,6 +6,9 @@ import {MemberDetailComponent} from "./components/members/member-detail/member-d
 import {ListsComponent} from "./components/lists/lists.component";
 import {MessagesComponent} from "./components/messages/messages.component";
 import {AuthGuard} from "./guards/auth.guard";
+import {TestErrorComponent} from "./errors/test-error/test-error.component";
+import {NotFoundComponent} from "./errors/not-found/not-found.component";
+import {ServerErrorComponent} from "./errors/server-error/server-error.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -19,7 +22,10 @@ const routes: Routes = [
       {path: 'messages', component: MessagesComponent},
     ]
   },
-  {path: '**', redirectTo: '', pathMatch: 'full'}
+  {path: 'errors', component: TestErrorComponent},
+  {path: 'not-found', component: NotFoundComponent},
+  {path: 'server-error', component: ServerErrorComponent},
+  {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 ];
 
 @NgModule({
