245ceb9191a3a8955b29e9948652e4338afaa02e
Anna <nebesna.kpi@gmail.com>
2023-08-14 23:01:53 +0200

    Pre-deploy

--- DatingAppAPI/Data/Seed.cs
+++ DatingAppAPI/Data/Seed.cs
@@ -8,6 +8,11 @@ namespace DatingAppAPI.Data;
 
 public class Seed
 {
+    public static async Task ClearConnections(DataContext context)
+    {
+        context.Connections.RemoveRange(context.Connections);
+        await context.SaveChangesAsync();
+    }
     public static async Task SeedUsers(UserManager<User> userManager, RoleManager<Role> roleManager)
     {
         if (await userManager.Users.AnyAsync()) return;
@@ -28,6 +33,8 @@ public class Seed
         {
             user.Photos.First().IsApproved = true;
             user.UserName = user.UserName.ToLower();
+            user.Created = DateTime.SpecifyKind(user.Created, DateTimeKind.Utc);
+            user.LastActive = DateTime.SpecifyKind(user.LastActive, DateTimeKind.Utc);
             await userManager.CreateAsync(user, "Pa$$w0rd");
             await userManager.AddToRoleAsync(user, "Member");
         }
